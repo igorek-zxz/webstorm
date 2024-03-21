61a38c95105dea0c17b9675cbc68aa2c3dca127c
Igor Kazaev <igorek.zxz@gmail.com>
2024-03-22 00:50:03 +0300

    Сверстана первая линия шапки

--- header.php
+++ header.php
@@ -7,6 +7,47 @@
 </head>
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <header>
-
+    <header class="storm-header">
+        <div class="storm-header__row storm-header__row-first">
+            <div class="storm-header__container">
+                <div class="storm-header__left">
+                    <a href="mailto:info@yandex.ru" class="storm-header__contact-link">
+                        <div class="storm-header__icon">
+                            <?php echo webstorm_get_svg( array( 'icon' => 'mail' ) ); ?>
+                        </div>
+                        <span class="storm-header__text">info@yandex.ru</span>
+                    </a>
+                    <a href="https://yandex.ru/maps/43/kazan/" rel="nofollow" target="_blank" class="storm-header__contact-link">
+                        <div class="storm-header__icon">
+                            <?php echo webstorm_get_svg( array( 'icon' => 'mapmarker' ) ); ?>
+                        </div>
+                        <span class="storm-header__text">Казань, ул. Декабристов, д.56, оф.301</span>
+                    </a>
+                    <div class="storm-header__contact-link">
+                        <div class="storm-header__icon">
+                            <?php echo webstorm_get_svg( array( 'icon' => 'clock' ) ); ?>
+                        </div>
+                        <span class="storm-header__text">Пн - Пт: 8:00 - 17:00</span>
+                    </div>
+                </div>
+                <div class="storm-header__right">
+                    <a href="tel:+78432221111" class="storm-header__contact-link">
+                        <div class="storm-header__icon">
+                            <?php echo webstorm_get_svg( array( 'icon' => 'phone' ) ); ?>
+                        </div>
+                        <span class="storm-header__text">(843) 222-11-11</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="storm-header__row storm-header__sticky">
+            <div class="storm-header__container">
+                <div class="storm-header__left">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="storm-header__logo-link">
+                        LOGO
+                    </a>
+                </div>
+                <div class="storm-header__right">MENU</div>
+            </div>
+        </div>
     </header>
